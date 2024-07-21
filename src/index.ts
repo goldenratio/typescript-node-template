d00996f4dd5d21cb964e62749bb517166555566c
fermi <fermi@fedora.lan>
2024-07-21 15:42:33 +0300

    clenaup

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { add } from "./util.js";
 
 // env usage
 console.log(`server name: ${process.env.SERVER_NAME}`);
+console.log(`env: ${process.env.NODE_ENV}`);
 console.log(add(40, 2));
 
 const a = 2;
